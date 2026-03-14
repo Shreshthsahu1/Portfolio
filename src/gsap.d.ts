056ffc1fa2171e6621311ef40a9502df8daeabca
Shreshth Sahu <shreshthsahu111@gmail.com>
2026-03-14 23:33:28 +0530

    Replace gsap-trial with gsap

--- src/gsap.d.ts
+++ src/gsap.d.ts
@@ -1,10 +1,10 @@
-declare module "gsap-trial/SplitText" {
+declare module "gsap/SplitText" {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   export const SplitText: any;
   export default SplitText;
 }
 
-declare module "gsap-trial/ScrollSmoother" {
+declare module "gsap/ScrollSmoother" {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   export const ScrollSmoother: any;
   export default ScrollSmoother;
